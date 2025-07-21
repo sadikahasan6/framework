b1c14ac1a6668ef111edb23e130932382b2d1632
sadikahasan6 <sadikahasan6@gmail.com>
2025-07-21 17:41:24 +0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Button from "$lib/components/ui/button.svelte";
+
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button type="button" size="md" variant="destructive" isLoading>Button</Button>
